970ff01e0d7781defe306504cdf3cfbe4968eb04
Gautam Adhikari <gautamadhikari911@gmail.com>
2026-05-08 16:16:45 +0530

    title change

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -2,8 +2,8 @@
 const Hero = () => {
   return (
     <section className="text-white">
-      <h1 className="text-3xl md:text-5xl font-headline font-bold leading-tight">
-        Exclusive Furniture Collections Available in Siliguri
+      <h1 className="text-3xl md:text-4xl font-headline font-bold leading-tight">
+        Exclusive Handcrafted Luxury Furniture from Jodhpur, <br /> Available in Siliguri
       </h1>
       <p className="mt-4 text-xl md:text-2xl font-headline text-accent">
            Explore 1000+ thoughtfully crafted designs with factory direct pricing, premium materials, and export quality craftsmanship all under one roof.
