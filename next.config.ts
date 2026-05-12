ed7c01120f0021c70e0889ba59d7002233b05286
Gautam Adhikari <gautamadhikari911@gmail.com>
2026-05-12 12:26:49 +0530

    change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/Siliguri-furniture',
+  basePath: '/siliguri-furnitur',
   skipTrailingSlashRedirect: true,
   /* config options here */
   typescript: {
