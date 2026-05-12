a89bb3467274603b0e9ed53f85eaea0d2738dcb4
Gautam Adhikari <gautamadhikari911@gmail.com>
2026-05-12 12:38:10 +0530

    missing e

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/siliguri-furnitur',
+  basePath: '/siliguri-furniture',
   skipTrailingSlashRedirect: true,
   /* config options here */
   typescript: {
